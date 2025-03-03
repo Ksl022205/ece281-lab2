f82673de6789be522b6f6df6dd8d8d0a52bee8a9
Ksl022205 <c27kyle.litman@afacademy.af.edu>
2025-03-02 20:27:23 -0700

    Update sevenseg_decoder_tb.vhd

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -18,27 +18,37 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity sevenseg_decoder_tb is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder_tb;
 
 architecture Behavioral of sevenseg_decoder_tb is
+    signal i_Hex : STD_LOGIC_VECTOR (3 downto 0);
+    signal o_seg_n : STD_LOGIC_VECTOR (6 downto 0);
+
+    -- Instantiate the Unit Under Test (UUT)
+    component sevenseg_decoder
+        Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+               o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
+    end component;
 
 begin
+    uut: sevenseg_decoder port map (
+        i_Hex => i_Hex,
+        o_seg_n => o_seg_n
+    );
 
-
+    process
+    begin
+        -- Iterate through all possible 4-bit values (0 to F in hex)
+        for i in 0 to 15 loop
+            i_Hex <= std_logic_vector(to_unsigned(i, 4));
+            wait for 10 ns; -- Wait time for simulation to observe changes
+        end loop;
+        
+        wait; -- Stop simulation
+    end process;
 end Behavioral;
