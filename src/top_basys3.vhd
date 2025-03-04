64be826d23abd13861155b967c63b4535ffd63b5
Ksl022205 <c27kyle.litman@afacademy.af.edu>
2025-03-03 19:09:45 -0700

    Update top_basys3.vhd

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -54,6 +54,7 @@
 --|    s_<signal name>          = state name
 --|
 --+----------------------------------------------------------------------------
+
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
